e3167be03874a663f5a41699865eb379320c177c
Nick Shym <shuminskiy23@gmail.com>
2023-08-10 14:22:10 +0300

    new text

--- src/App.js
+++ src/App.js
@@ -4,44 +4,45 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div>
+        <h1>Privacy Policy for Medical App with Chat Feature</h1>
         <p>
-          Privacy Policy for Medical App with Chat Feature
-
-          This privacy policy outlines how our medical application with a chat feature (hereinafter referred to as the "App") collects, uses, and safeguards your data when using the chat functionality. Please carefully review this policy before using the App.
-
-          1. Collected Information
-
-          We only collect the minimal necessary information required to facilitate the chat functionality within the App. This may include your name, email address, and any other data you provide during the registration process.
-
-          2. Use of Information
-
-          The information you provide through the chat in the App is strictly used for medical consultations and communication between you and medical professionals. We adhere strictly to privacy norms and do not disclose your information to third parties except as required by law.
-
-          3. Security
-
-          We take all necessary measures to protect your information from unauthorized access, use, or disclosure. All data is transmitted through encrypted connections, and we maintain high-security standards.
-
-          4. Data Storage
-
-          Your data is stored on our secure servers for the necessary duration required to provide quality medical consultation and support.
-
-          5. Your Rights
-
-          You can request access to your information, correction, or deletion of data, as well as inquire about our privacy policy by contacting us at the provided contacts.
-
-          We uphold the highest standards of data processing and privacy protection. If you have any questions or concerns regarding our privacy policy or the use of the App, please reach out to us.
-
-          Contacts:
-
-          Nikita
-          dl_nik@mail.ru
-
-          This privacy policy is effective as of 0.08.2023. We reserve the right to make changes to this policy and will notify users accordingly.
-
-          Last Updated: 10.08.2023
+          This privacy policy outlines how our medical application with a chat feature (hereinafter referred to as the "Sos") collects, uses, and safeguards your data when using the chat functionality. Please carefully review this policy before using the App.
         </p>
-      </header>
+        <h2>1. Collected Information</h2>
+        <p>
+          We only collect the minimal necessary information required to facilitate the chat functionality within the App. This may include your name, email address, and any other data you provide during the registration process.
+        </p>
+        <h2>2. Use of Information</h2>
+        <p>
+          The information you provide through the chat in the App is strictly used for medical consultations and communication between you and medical professionals. We adhere strictly to privacy norms and do not disclose your information to third parties except as required by law.
+        </p>
+        <h2>3. Security</h2>
+        <p>
+          We take all necessary measures to protect your information from unauthorized access, use, or disclosure. All data is transmitted through encrypted connections, and we maintain high-security standards.
+        </p>
+        <h2>4. Data Storage</h2>
+        <p>
+          Your data is stored on our secure servers for the necessary duration required to provide quality medical consultation and support.
+        </p>
+        <h2>5. Your Rights</h2>
+        <p>
+          You can request access to your information, correction, or deletion of data, as well as inquire about our privacy policy by contacting us at the provided contacts.
+        </p>
+        <p>
+          We uphold the highest standards of data processing and privacy protection. If you have any questions or concerns regarding our privacy policy or the use of the App, please reach out to us.
+        </p>
+        <h2>Contacts:</h2>
+        <p>
+          Nick<br />
+          dl_nik@mail.ru<br />
+
+        </p>
+        <p>
+          This privacy policy is effective as of 10/08/2023. We reserve the right to make changes to this policy and will notify users accordingly.
+        </p>
+        <p>Last Updated: 10/08/2023</p>
+      </div>
     </div>
   );
 }
